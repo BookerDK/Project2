2421cacf660b812fcbd5659d211abd9146b87811
BookerDK <162234690+BookerDK@users.noreply.github.com>
2024-04-16 21:21:26 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+let userController = new UserController("form-user-create", "table-users"); //this object instantiates a new class of UserController, and send the attributes of the form id html 
+
+
+
+
+
